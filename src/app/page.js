6cd5cae5b14b7ce5d44af0d4323ff5f2c0cf46eb
Alain Iranzi <alainiranzi00@gmail.com>
2025-12-22 21:13:58 +0200

    navugating between pages

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
-export default function Home() {
-  console.log("executing...");
+import Link from "next/link";
 
+export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="Logo" width={200} height={200} />
-      <h1>Welcome to this nextJS app</h1>
-      <p>let&apos;s get started</p>
+      <h1>Welcome to this Next.js app</h1>
+      <p>Let&apos;s get started</p>
+      <p>
+        <Link href="/about">About Us</Link>
+      </p>
     </main>
   );
 }
