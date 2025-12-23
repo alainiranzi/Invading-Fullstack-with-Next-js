50a050561c2a30c7f6ab7e037fce5b85b0e0e921
Alain Iranzi <alainiranzi00@gmail.com>
2025-12-23 17:43:32 +0200

    worked on exrcises of how to make routes and dynamic ones

--- components/header.js
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <>
-      <img src="/logo.png" alt="Logo" width={50} height={50} />
-      <h1>Welcome to this Next.js Course</h1>
-    </>
-  );
-}
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
-import Header from "../../components/header";
 
 export default function Home() {
   return (
     <main>
-      <Header />
-      <p>Let's get started</p>
-      <p><Link href="/about">About Us</Link></p>
+      <h1>
+        Time to NextLevel Up
+        </h1>
+      <p><Link href='/meals'> Meals page</Link> </p>
+      <p><Link href='/meals/share'>Our meals</Link></p>
+      <p><Link href='/community'>Community</Link></p>
     </main>
   );
 }
