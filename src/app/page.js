8b1739c0c90941088ff9e69da0252ed2435e0616
Alain Iranzi <alainiranzi00@gmail.com>
2025-12-23 08:43:38 +0200

    working with pages and layouts

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
+    
     <main>
-      <img src="/logo.png" alt="Logo" width={200} height={200} />
       <h1>Welcome to this Next.js app</h1>
       <p>Let&apos;s get started</p>
       <p>
