2fbfca1c2db0ed2a7dc5f0f184cd911e495eccc4
Alain Iranzi <alainiranzi00@gmail.com>
2025-12-31 12:14:50 +0200

    using client components

--- src/app/components/main-header/main-header.js
+++ src/app/components/main-header/main-header.js
@@ -1,24 +1,31 @@
+'use client';
+
 import Link from "next/link";
 import Image from "next/image";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
 import ImageSlideshow from "../images/image-slideshow";
+import NavLink from "./nav-link";
 
 export default function MainHeader() {
   return (
     <>
       <MainHeaderBackground />
+
       <header className={classes.header}>
+        {/* Slideshow */}
         <ImageSlideshow />
 
+        {/* Hero */}
         <div className={classes.hero}>
           <h1>Next food for next level foodies</h1>
           <p>Taste & share food from all over the world</p>
         </div>
 
+        {/* Navigation / CTA */}
         <div className={classes.cta}>
-          <Link href="/meals">Explore foodies</Link>
-          <Link href="/community">Join the community</Link>
+          <NavLink href="/meals">Explore foodies</NavLink>
+          <NavLink href="/community">Join the community</NavLink>
         </div>
       </header>
     </>
