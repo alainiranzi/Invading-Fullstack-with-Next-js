28a54e346d161bfb36ecd1c78bf8402c77e70e8b
Alain Iranzi <alainiranzi00@gmail.com>
2026-01-02 23:25:44 +0200

    espanded initdb

--- initdb.js
+++ initdb.js
@@ -6,7 +6,11 @@ const dummyMeals = [
     title: 'Juicy Cheese Burger',
     slug: 'juicy-cheese-burger',
     image: '/images/burger.jpeg',
-    summary
+    summary:
+    'A mouth-watering burger with a juicy beef patty and melted'
+    instructions: `
+    1. Prepare the patty:
+    Mix 200g of ground beef with salt and pepper. form........`
 }
 
 ]
