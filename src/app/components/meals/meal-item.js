675eb64d63f47256ffed1d29c00da916ce5d879a
Alain Iranzi <alainiranzi00@gmail.com>
2026-01-04 13:28:20 +0200

    worked on meal items

--- src/app/components/meals/meal-item.js
+++ src/app/components/meals/meal-item.js
@@ -2,29 +2,24 @@ import Link from 'next/link';
 import Image from 'next/image';
 import classes from './meal-item.module.css';
 
-export default function MealItem({
-  title,
-  slug,
-  image,
-  summary,
-  creator,
-}) {
+export default function MealItem({ title, slug, image, summary, creator }) {
   return (
-    <article className={classes.meal}>
-      <header className={classes.header}>
-        <Image src={image} alt={title} fill />
-        <div className={classes.headerText}>
-          <h2>{title}</h2>
-          <p>{creator}</p>
-        </div>
-      </header>
-
+    <li className={classes.meal}>
+      <div className={classes.imageWrapper}>
+        <Image src={image} alt={title} fill className={classes.image} />
+      </div>
+      <div className={classes.headerText}>
+        <h2>{title}</h2>
+        <p>{creator}</p>
+      </div>
       <div className={classes.content}>
         <p className={classes.summary}>{summary}</p>
         <div className={classes.actions}>
-          <Link href={`/meals/${slug}`}>View Details</Link>
+          <Link href={`/meals/${slug}`} className={classes.link}>
+            View Details
+          </Link>
         </div>
       </div>
-    </article>
+    </li>
   );
 }
--- src/app/components/meals/meals-grid.js
+++ src/app/components/meals/meals-grid.js
@@ -4,10 +4,8 @@ import classes from './meals-grid.module.css';
 export default function MealsGrid({ meals }) {
   return (
     <ul className={classes.meals}>
-      {meals.map(meal => (
-        <li key={meal.id}>
-          <MealItem {...meal} />
-        </li>
+      {meals.map((meal) => (
+        <MealItem key={meal.id} {...meal} />
       ))}
     </ul>
   );
