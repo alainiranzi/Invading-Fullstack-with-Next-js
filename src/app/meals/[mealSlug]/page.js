df06a83119d2d79ed172c62b00bf17364235cf4b
Alain Iranzi <alainiranzi00@gmail.com>
2026-01-07 15:59:58 +0200

    working still

--- src/app/meals/[mealSlug]/page.js
+++ src/app/meals/[mealSlug]/page.js
@@ -1,35 +1,16 @@
 import Image from 'next/image';
+
 import classes from './page.module.css';
-import { getMeals } from '@/app/lib/meals';
 
-export default async function MealDetailPage({ params }) {
-  if (!params || !params.mealSlug) {
-    return <p>Invalid route</p>;
-  }
+export default function MealsDetailsPage() {
+    return <>
+      <header className={classes.header}>
+      <div className={classes.image}>
+        <Image />
 
-  const mealSlug = params.mealSlug;
+      </div>
+      </header>
+      <main></main>
 
-  const meals = await getMeals();
-  const meal = meals.find((meal) => meal.slug === mealSlug);
-
-  if (!meal) {
-    return <p>Meal not found</p>;
-  }
-
-  return (
-    <main className={classes.main}>
-      <h1>{meal.title}</h1>
-
-      <Image
-        src={meal.image}
-        alt={meal.title}
-        width={600}
-        height={350}
-      />
-
-      <p>{meal.summary}</p>
-
-      <pre>{meal.instructions}</pre>
-    </main>
-  );
+      </>
 }
