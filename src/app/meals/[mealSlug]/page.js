efa3451cf9765f2c64b6e97bc6a8d1f3f022ddd5
Alain Iranzi <alainiranzi00@gmail.com>
2026-01-10 21:10:02 +0200

    loading and renderingmeal details with via dinamic

--- src/app/meals/[mealSlug]/page.js
+++ src/app/meals/[mealSlug]/page.js
@@ -1,16 +1,36 @@
 import Image from 'next/image';
-
 import classes from './page.module.css';
 
 export default function MealsDetailsPage() {
-    return <>
+  return (
+    <>
       <header className={classes.header}>
-      <div className={classes.image}>
-        <Image />
+        <div className={classes.image}>
+          <Image
+            src="/images/pizza.jpeg"   // 🔑 src irakenewe
+            alt="Meal image"
+            fill
+            className={classes.img}
+          />
+        </div>
 
-      </div>
+        <div className={classes.headerText}>
+          <h1>Delicious Pizza</h1>
+
+          <p className={classes.creator}>
+            by <a href="mailto:max@example.com">Max</a>
+          </p>
+
+          <p className={classes.summary}>
+            A cheesy pizza topped with fresh tomatoes and basil.
+          </p>
+        </div>
       </header>
-      <main></main>
 
-      </>
+      <main className={classes.instructions}>
+        <h2>Instructions</h2>
+        <p>Cook with love ❤️</p>
+      </main>
+    </>
+  );
 }
