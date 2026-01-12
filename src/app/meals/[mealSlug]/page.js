cb66729a1cd0c38c8c16b31de53490286a757366
Alain Iranzi <alainiranzi00@gmail.com>
2026-01-12 12:08:15 +0200

    still loading and rendering meals with dynamic

--- src/app/meals/[mealSlug]/page.js
+++ src/app/meals/[mealSlug]/page.js
@@ -2,38 +2,28 @@ import Image from 'next/image';
 import { getMeal } from '@/app/lib/meals';
 import classes from './page.module.css';
 
-export default async function MealsDetailsPage({ params }) {
-  
-  const meal = await getMeal(params.mealSlug);
-
-  if (!meal) {
-    return <p className={classes.notFound}>Meal not found</p>;
-  }
+export default function MealsDetailPage({params}) {
+const meal = getMeal(params.mealSlug)
 
   return (
-    <main className={classes.main}>
+    <>
       <header className={classes.header}>
-        <div className={classes.image}>
-          <Image
-            src={meal.image}  
-            alt={meal.title}
-            fill
-            style={{ objectFit: 'cover' }}
-          />
-        </div>
-        <div className={classes.headerText}>
-          <h1>{meal.title}</h1>
-          <p className={classes.creator}>
-            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+      <div className={classes.image}>
+       <Image fill/>
+      </div>
+      <div className={classes.headerText}>
+        <h1>Meal Title</h1>
+        <p className={classes.creator}>
+          by <a href={`mailto:${'EMAIL'}`}>NAME</a>
           </p>
-          <p className={classes.summary}>{meal.summary}</p>
+          <p className={classes.summary}>SUMMARY</p>
         </div>
       </header>
-
-      <section className={classes.instructions}>
-        <h2>Instructions</h2>
-        <pre>{meal.instructions}</pre>
-      </section>
-    </main>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{
+          __html: '...',
+        }}></p>
+      </main>
+    </>
   );
 }
