9f9442e05334c1349caf507ecf29b0e287a872cc
Alain Iranzi <alainiranzi00@gmail.com>
2026-01-18 14:52:09 +0200

    added an image preview on the picker

--- src/app/components/meals/image-picker.js
+++ src/app/components/meals/image-picker.js
@@ -1,19 +1,48 @@
 'use client';
 
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+
 import classes from './image-picker.module.css';
 
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState(null);
   const imageInput = useRef();
 
   function handlePickClick() {
     imageInput.current.click();
   }
 
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet</p>}
+          {pickedImage && (
+            <img
+              src={pickedImage}
+              alt="Selected"
+              className={classes.previewImage}
+            />
+          )}
+        </div>
+
         <input
           className={classes.input}
           type="file"
@@ -21,7 +50,9 @@ export default function ImagePicker({ label, name }) {
           accept="image/png,image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
         />
+
         <button
           className={classes.button}
           type="button"
