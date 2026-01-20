79d4cad6e11ece491f37ff472e887ece8b388af6
Alain Iranzi <alainiranzi00@gmail.com>
2026-01-20 13:40:00 +0200

    working on storing uploaded images and storing data in the database

--- src/app/lib/meals.js
+++ src/app/lib/meals.js
@@ -1,4 +1,8 @@
+import fs from 'node: fs';
+
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
 
 const db = sql('meals.db');
 
@@ -8,4 +12,46 @@ export async function getMeals() {
 
 export async function getMeal(slug) {
   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
+
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split('.').pop();
+  const fileName = `${meal.slug}.${extension}`
+
+  const stream = fs.createWriteStream(`public/images/meals/${fileName}`)
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(buffer.from(bufferedImage)), (error) => {
+    if (error) {
+      throw new Error('Saving image failed!');
+}
+  };
+
+  meal.image  = `/images/meals/${fileName}`
+  db.prepare(`
+    INSERT INTO meals 
+    (
+      title,
+      slug,
+      image,
+      instructions,
+      summary,
+      creator,
+      creator_email,
+      slug
+    ) 
+    VALUES 
+    @title,
+    @slug,
+    @image,
+    @instructions,
+    @summary,
+    @creator,
+    @creator_email,
+    @slug
+  `)
+  );
 }
