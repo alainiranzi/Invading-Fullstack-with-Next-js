9d449e780ef44011be0e16a573de68fd21f59640
Alain Iranzi <alainiranzi00@gmail.com>
2026-02-03 01:09:32 +0200

    returned back to the form and fixing browse meals button

--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,13 +1,71 @@
-import { getMeals } from '@/app/lib/meals';
-import MealsGrid from '@/app/components/main-header/meals/meals-grid';
+import ImagePicker from "@/app/components/main-header/meals/image-picker";
+import { shareMeal } from "@/app/lib/actions";
+import Link from "next/link";
+import classes from "page.module.css";
 
-export default async function MealsPage() {
-  const meals = await getMeals();
-
+export default function ShareMealsPage() {
   return (
-    <main>
-      <h1>All Shared Meals</h1>
-      <MealsGrid meals={meals} />
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Share Your <span className={classes.highlight}>Delicious Meals</span>
+        </h1>
+        <p>Or any other meal you feel needs sharing!</p>
+
+      
+        <div className={classes.nav}>
+          <Link href="/meals/share" className={classes.link}>
+            🍽 Share Your Meal
+          </Link>
+          <Link href="/community" className={classes.linkAlt}>
+            🤝 Join the Community
+          </Link>
+        </div>
+      </header>
+
+      <main className={classes.main}>
+        <form className={classes.form} action={shareMeal}>
+          <div className={classes.row}>
+            <p>
+              <label htmlFor="name">Your Name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+
+            <p>
+              <label htmlFor="email">Your Email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+
+          <p>
+            <label htmlFor="title">Meal Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows={8}
+              required
+            ></textarea>
+          </p>
+
+          <ImagePicker label="Meal Image" name="image" />
+
+          <div className={classes.actions}>
+            <button type="submit" className={classes.button}>
+              Share Meal
+            </button>
+          </div>
+        </form>
+      </main>
+    </>
   );
 }
